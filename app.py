12b0af7d1cc3ff05803364522afbd3a0e01d5bcd
Carys Mills <mills.carys@gmail.com>
2021-08-14 16:01:33 -0400

    filter viz data

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ import pandas as pd
 import plotly.graph_objects as go
 
 df = pd.read_csv('trudeau-output.tsv', sep='\t', header=0)
+filtered = df[df.word.eq('climate')]
 
-fig = go.Figure(go.Scatter(x = df['date'], y = df['value']))
+fig = go.Figure(go.Scatter(x = filtered['date'], y = filtered['value']))
 
-fig.update_layout(title='Trudeau tweets',
+fig.update_layout(title='Trudeau tweets: climate',
                    plot_bgcolor='rgb(230, 230,230)')
 
 fig.show()
+
+### to do: compare to overall tweet volume
+### add missing month values
+### lowercase for tallies, group like words
+### remove august since it is incomplete data
